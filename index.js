40cff10bae58df22d8ccedb56b92dace6b707a3a
gandanisean-xxd <tagle.seanandrei@gmail.com>
2025-04-28 16:02:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const router = express.Router();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://soft-eng-deploy.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
 app.use(express.json());
 
 // Connect to MongoDB 

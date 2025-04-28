7ffc0c8ec5e22653552a6eb566c64edd3d296d2b
gandanisean-xxd <tagle.seanandrei@gmail.com>
2025-04-28 13:31:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ import UserModel from './User.js';
 import bcrypt from 'bcryptjs';
 import AdminModel from './Admin.js';
 import SubmissionModel from './Submission.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,14 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://tagleseanandrei:QsIO8e1RvPqDDpwS@cluster0.tphbs0m.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://tagleseanandrei:<db_password>@cluster0.tphbs0m.mongodb.net/" + "test?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-});
+})
 
 app.post('/admin-login', async (req, res) => {
   const { username, password } = req.body;
@@ -304,12 +297,6 @@ app.post('/submissions', async (req, res) => {
   }
 });
   
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all route to serve the frontend's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
   
 // Test Route
 app.get('/api/hello', (req, res) => {

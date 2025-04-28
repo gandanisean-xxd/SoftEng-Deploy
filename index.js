3de766d275b2757b9635b8357f1c9771f2079cd5
gandanisean-xxd <tagle.seanandrei@gmail.com>
2025-04-28 16:12:00 +0800

    yes

--- index.js
+++ index.js
@@ -6,17 +6,11 @@ import bcrypt from 'bcryptjs';
 import AdminModel from './Admin.js';
 import SubmissionModel from './Submission.js';
 
-const backendUrl = "https://eco-urban.onrender.com";
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const router = express.Router();
 
-app.use(cors({
-  origin: ['https://soft-eng-deploy.vercel.app'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}));
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB 
